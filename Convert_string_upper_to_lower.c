dbc9faa6a268d93f58d53ba37221cf6605c0920c
Dhaval572 <dp9718776@gmail.com>
2024-10-27 10:07:15 +0530

    Update Convert_string_upper_to_lower.c

--- Convert_string_upper_to_lower.c
+++ Convert_string_upper_to_lower.c
@@ -1,31 +1,31 @@
-//upper case to lower case convertion
+// upper case to lower case convertion
 #include <stdio.h>
 #include <string.h>
 
-void TOLOWER(char *str1,char *str2)
+void TOLOWER(char *str1, char *str2)
 {
 	int i = 0;
-    while (str1[i] != '\0')
-    {
-        if (str1[i] >= 'A' && str1[i] <= 'Z')
-        {
-            str2[i] = str1[i] + 32; // Convert uppercase to lowercase
-        }
-        else
-        {
-            str2[i] = str1[i];
-        }
-        i++;
-    }
-    str2[i] = '\0';
+	while (str1[i] != '\0')
+	{
+		if (str1[i] >= 'A' && str1[i] <= 'Z')
+		{
+			str2[i] = str1[i] + 32; // Convert uppercase to lowercase
+		}
+		else
+		{
+			str2[i] = str1[i];
+		}
+		i++;
+	}
+	str2[i] = '\0';
 	printf("String converted into small (lower case): ");
-    puts(str2);
+	puts(str2);
 }
 
 void main()
 {
-	char str1[100],str2[100];
+	char str1[100], str2[100];
 	printf("Enter the string (In capital): ");
 	gets(str1);
-	TOLOWER(str1,str2);
+	TOLOWER(str1, str2);
 }
